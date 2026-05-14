cd918a8f77c5c903d45f8ee8b6d309bc3d07f3f5
Magomedali Gajiev <mag198421@gmail.com>
2026-05-14 20:59:35 +0300

    feat: добавлены модели CartItem и CartLine для корзины
    Добавлены классы CartItem и CartLine для хранения данных о товарах в корзине и автоматического расчета стоимости и старой цены. Изменение необходимо для реализации функционала корзины в интернет-магазине. Breaking changes отсутствуют.

---
+++ OnlineCoffeShop.Web/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineCoffeShop.Web.Models;
+
+public class CartItem
+{
+    public Guid ProductId { get; set; }
+    
+    public int Qty { get; set; }
+}
+
+public class CartLine
+{
+    private Product Product { get; init; } = null!;
+    
+    public  int Qty { get; init; }
+    
+    public  decimal LineTotal => Product.Price * Qty;
+    
+    public decimal LineOldTotal => (Product.OldPrice ?? Product.Price) * Qty;
+}
