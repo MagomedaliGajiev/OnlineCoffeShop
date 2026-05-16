8f7e258a10e94af182947e27fb4538cedc81b3a7
Магомедали Гаджиев <mag198421@gmail.com>
2026-05-16 18:22:33 +0300

    feat: добавлен контроллер FavController с действием Toggle
    Реализован новый контроллер для управления избранными товарами.
    Метод Toggle обрабатывает POST-запросы, добавляя или удаляя товар из избранного через репозиторий IFavoritesRepository.
    После выполнения пользователь перенаправляется на предыдущую страницу (если передан корректный returnUrl) или на главную.
    Breaking changes: нет.

---
+++ OnlineCoffeShop.Web/Controllers/FavController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineCoffeShop.Web.Repositories.Abstractions;
+
+namespace OnlineCoffeShop.Web.Controllers;
+
+public class FavController : Controller
+{
+    private readonly IFavoritesRepository _favs;
+
+    public FavController(IFavoritesRepository favs)
+    {
+        _favs = favs;
+    }
+
+    [HttpPost, ValidateAntiForgeryToken]
+    public IActionResult Toggle(Guid id, string? returnUrl = null)
+    {
+        _favs.Toggle(id);
+        return !string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl)
+            ? LocalRedirect(returnUrl)
+            : RedirectToAction("Index", "Home");
+    }
+}
